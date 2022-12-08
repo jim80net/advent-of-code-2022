5eff2ea1ad60c39c7b7444c47f2c6fb0a8cdba0f
Jim Park <jimp@scribd.com>
2022-12-07 18:40:07 -0800

    mix format

--- d6/lib/advent_2022.ex
+++ d6/lib/advent_2022.ex
@@ -8,40 +8,44 @@ defmodule Advent2022 do
     # IO.inspect("Evaluating #{charlist}")
     {first_thirteen, rest} = Enum.split(charlist, 13)
     initial_mem = Enum.reverse(first_thirteen)
+
     rest
     |> Enum.reduce_while({initial_mem, 12}, fn element, acc ->
-        {mem, count} = acc
-        # IO.inspect({element, mem, count})
-        if [element | mem] == Enum.uniq([element | mem]) do
-          {:halt, count + 2}
-        else
-          newmem = [element | mem]
+      {mem, count} = acc
+      # IO.inspect({element, mem, count})
+      if [element | mem] == Enum.uniq([element | mem]) do
+        {:halt, count + 2}
+      else
+        newmem =
+          [element | mem]
           |> Enum.take(13)
-          {:cont, {newmem, count + 1}}
-        end
-      end)
-  end
- 
 
+        {:cont, {newmem, count + 1}}
+      end
+    end)
+  end
 
   def part1(charlist) do
     # IO.inspect("Evaluating #{charlist}")
     {first_three, rest} = Enum.split(charlist, 3)
     initial_mem = Enum.reverse(first_three)
+
     rest
     |> Enum.reduce_while({initial_mem, 2}, fn element, acc ->
-        {mem, count} = acc
-        # IO.inspect({element, mem, count})
-        if [element | mem] == Enum.uniq([element | mem]) do
-          {:halt, count + 2}
-        else
-          newmem = [element | mem]
+      {mem, count} = acc
+      # IO.inspect({element, mem, count})
+      if [element | mem] == Enum.uniq([element | mem]) do
+        {:halt, count + 2}
+      else
+        newmem =
+          [element | mem]
           |> Enum.take(3)
-          {:cont, {newmem, count + 1}}
-        end
-      end)
+
+        {:cont, {newmem, count + 1}}
+      end
+    end)
   end
- 
+
   def parse_input(stream) do
     stream
     |> Stream.map(&String.trim/1)
@@ -66,8 +70,7 @@ defmodule Advent2022 do
   main function
   """
   def start(_type, _args) do
-    children = [
-    ]
+    children = []
 
     processes =
       Supervisor.start_link(children, strategy: :one_for_one, name: Advent2022.Supervisor)
@@ -78,7 +81,7 @@ defmodule Advent2022 do
 
   def main do
     Application.ensure_all_started(:advent_2022)
-    
+
     read_input('input')
     |> parse_input()
     |> part1()
@@ -89,8 +92,6 @@ defmodule Advent2022 do
     |> part2()
     |> IO.inspect()
 
-
     Application.stop(:advent_2022)
   end
-
 end
